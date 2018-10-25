18cb0b09125435808776682393f568a8eb58f643
Taylor-Cameron <taylorcameron95@hotmail.ca>
2018-10-25 17:53:52 +0000

    print index of waldo when found

--- findWaldo.js
+++ findWaldo.js
@@ -1,8 +1,7 @@
-// The second argument/parameter is expected to be a function
 function findWaldo(list, found) {
   for (var i = 0; i < list.length; i++) {
     if (list[i] === "Waldo") {
-      found(i);   // execute callback
+      found(i);
     }
   }
 }
@@ -11,4 +10,4 @@ function actionWhenFound(index) {
     console.log("Found Waldo at index " + index + '!');
 }
 
-findWaldo(["Alice", "Bob", "Waldo", "Winston"], actionWhenFound);
+// findWaldo(["Alice", "Bob", "Waldo", "Winston"], actionWhenFound);
